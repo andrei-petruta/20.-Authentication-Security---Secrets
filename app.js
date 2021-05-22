e7151bf7f498062c8339fb2a8da982010ca19874
Andrei Petruta <aandrei.petruta@gmail.com>
2021-05-22 18:54:29 +0300

    add submit functionality based on different users

--- app.js
+++ app.js
@@ -44,7 +44,8 @@ mongoose.set('useCreateIndex', true);
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 })
 
 // userSchema.plugin(encrypt, {secret: process.env.secret, encryptedFields: ["password"]});
@@ -93,16 +94,48 @@ app.get("/register", function(req, res) {
     res.render("register");
 });
 app.get("/secrets", function(req, res) {
-    if (req.isAuthenticated()) {
-        res.render("secrets");
-    } else {
-        res.redirect("login");
-    }
+    User.find({"secret": {$ne: null}}, function(err, foundUsers) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUsers) {
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
+        }
+    })
 });
 app.get("/logout", function(req, res) {
     req.logout();
     res.redirect("/");
 })
+
+
+
+app.get("/submit", function(req, res) {
+    if (req.isAuthenticated()) {
+        res.render("submit");
+    } else {
+        res.redirect("login");
+    }
+});
+app.post("/submit", function(req, res) {
+    const submittedSecret = req.body.secret;
+
+    console.log(req.user.id);
+
+    User.findById(req.user.id, function(err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function() {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    })
+});
 
 
 app.get("/auth/google", passport.authenticate("google", {scope: ["profile"]}));
